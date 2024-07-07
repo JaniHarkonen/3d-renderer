7b63b468334b4757926106c534682c863709fa23
JaniHarkonen <jani.harkonen.real@gmail.com>
2024-07-07 15:19:55 +0300

    Created window, renderer and program loop.

---
+++ 3drenderer/src/main/java/project/Application.java
@@ -0,0 +1,23 @@
+package project;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Application app = new Application();
+		app.execute();
+	}
+	
+	public void execute() {
+		Window window = new Window(800, 600);
+		window.setRenderer(new Renderer());
+		window.init();
+		
+		while( !window.closeIfNeeded() ) {
+			window.pollInputEvents();
+			window.swapBuffers();
+		}
+		
+		
+		System.out.println("main loop terminated!");
+	}
+}
---
+++ 3drenderer/src/main/java/project/Window.java
@@ -0,0 +1,66 @@
+package project;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.system.MemoryUtil;
+
+public class Window {
+	private int width;
+	private int height;
+	private long windowID;
+	private Renderer renderer;
+	
+	public Window(int width, int height) {
+		this.windowID = MemoryUtil.NULL;
+		this.width = width;
+		this.height = height;
+		this.renderer = null;
+	}
+	
+	public void init() {
+		GLFW.glfwInit();
+		
+		this.windowID = GLFW.glfwCreateWindow(this.width, this.height, "3D renderer", MemoryUtil.NULL, MemoryUtil.NULL);
+		
+		if( this.windowID == MemoryUtil.NULL ) {
+			System.out.println("Unable to create window!");
+			return;
+		}
+		
+		GLFWVidMode videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+		GLFW.glfwSetWindowPos(this.windowID, videoMode.width() / 2 - this.width / 2, videoMode.height() / 2 - this.height / 2);
+		
+		GLFW.glfwSetKeyCallback(this.windowID, (window, key, scancode, action, mods) -> {
+			if( key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_PRESS ) {
+				GLFW.glfwSetWindowShouldClose(this.windowID, true);
+			}
+		});
+		
+		GLFW.glfwMakeContextCurrent(this.windowID);
+		GLFW.glfwSwapInterval(1); // v-sync
+		this.renderer.init();
+	}
+	
+	public void pollInputEvents() {
+		GLFW.glfwPollEvents();
+	}
+	
+	public void swapBuffers() {
+		GLFW.glfwSwapBuffers(this.windowID);
+		this.renderer.render();
+	}
+	
+	public boolean closeIfNeeded() {
+		if( GLFW.glfwWindowShouldClose(this.windowID) ) {
+			GLFW.glfwDestroyWindow(this.windowID);
+			GLFW.glfwTerminate();
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void setRenderer(Renderer renderer) {
+		this.renderer = renderer;
+	}
+}
