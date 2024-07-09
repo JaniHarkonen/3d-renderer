e75ac745281dfef8e02d306d0f263870f9d17212
JaniHarkonen <jani.harkonen.real@gmail.com>
2024-07-09 15:38:30 +0300

    Offloaded more rendering logic onto Window, added Scene.

--- 3drenderer/src/main/java/project/Application.java
+++ 3drenderer/src/main/java/project/Application.java
@@ -11,28 +11,15 @@ public class Application {
 		final String TITLE = "3D Renderer";
 		final int FPS_MAX = 60;
 		
-		Window window = new Window(TITLE, 800, 600);
+		Window window = new Window(TITLE, 800, 600, FPS_MAX, 0);
 		window.setRenderer(new Renderer(window));
 		window.init();
 		
-		long time = System.nanoTime();
-		long frameTime = System.nanoTime();
-		long frameDelta = 1000000000 / FPS_MAX;
-		int counter = 0;
-		while( !window.closeIfNeeded() ) {
-			if( System.nanoTime() - frameTime >= frameDelta ) {
-				frameTime = System.nanoTime();
-				
-				window.pollInputEvents();
-				window.swapBuffers();
-				counter++;
-				
-				if( System.nanoTime() - time >= 1000000000 ) {
-					window.setTitle(TITLE + " | FPS: " + counter + "/" + FPS_MAX);
-					counter = 0;
-					time = System.nanoTime();
-				}
-			}
+		Scene scene = new Scene();
+		
+		while( !window.isDestroyed() ) {
+			window.refresh();
+			scene.update();
 		}
 		
 		System.out.println("main loop terminated!");
