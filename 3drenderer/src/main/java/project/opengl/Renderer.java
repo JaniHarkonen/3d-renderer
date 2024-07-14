e23cd3c01e54f8d234a1679061972fb38d6660a5
JaniHarkonen <jani.harkonen.real@gmail.com>
2024-07-14 22:43:16 +0300

    ShaderPrograms are now generic.

--- 3drenderer/src/main/java/project/opengl/Renderer.java
+++ 3drenderer/src/main/java/project/opengl/Renderer.java
@@ -5,18 +5,32 @@ import org.lwjgl.opengl.GL46;
 
 import project.Window;
 import project.asset.Mesh;
+import project.scene.ASceneObject;
 import project.scene.Camera;
 import project.scene.Model;
 import project.scene.Scene;
-import project.scene.ASceneObject;
+import project.shader.Shader;
 import project.shader.ShaderProgram;
 
 public class Renderer {
 	
+		// Uniform names
+	private static final String U_DIFFUSE_SAMPLER = "uDiffuseSampler";
+	private static final String U_PROJECTION = "uProjection";
+	private static final String U_CAMERA_TRANSFORM = "uCameraTransform";
+	private static final String U_OBJECT_TRANSFORM = "uObjectTransform";
+	
+	private static final String U_PROJECTION_GUI = "uProjection";
+	private static final String U_DIFFUSE_SAMPLER_GUI = "uDiffuseSampler";
+	private static final String U_OBJECT_TRANSFORM_GUI = "uObjectTransform";
+	private static final String U_HAS_TEXTURE_GUI = "uHasTexture";
+	private static final String U_ELEMENT_COLOR_GUI = "uElementColor";
+	private static final String U_TEXT_COLOR_GUI = "uTextColor";
+	
 	private Window clientWindow;
 	
 	private ShaderProgram shaderProgram;
-	private ShaderProgram guiShaderProgram;
+	private ShaderProgram shaderProgramGUI;
 	
 	private VAOCache vaoCache;
 	private TextureCache textureCache;
@@ -25,6 +39,7 @@ public class Renderer {
 	public Renderer(Window clientWindow, Scene scene) {
 		this.clientWindow = clientWindow;
 		this.shaderProgram = null;
+		this.shaderProgramGUI = null;
 		this.vaoCache = null;
 		this.textureCache = null;
 		this.scene = scene;
@@ -33,11 +48,28 @@ public class Renderer {
 	public void init() {
 		GL.createCapabilities();
 		GL46.glClearColor(0.85f, 0.85f, 0.85f, 0.0f);
-		GL46.glEnable(GL46.GL_DEPTH_TEST);
 		
-			// Shaders
+			// Scene shaders
 		this.shaderProgram = new ShaderProgram();
+		this.shaderProgram.declareUniform(Renderer.U_DIFFUSE_SAMPLER);
+		this.shaderProgram.declareUniform(Renderer.U_PROJECTION);
+		this.shaderProgram.declareUniform(Renderer.U_CAMERA_TRANSFORM);
+		this.shaderProgram.declareUniform(Renderer.U_OBJECT_TRANSFORM);
+		this.shaderProgram.addShader(new Shader("default.vert", GL46.GL_VERTEX_SHADER));
+		this.shaderProgram.addShader(new Shader("default.frag", GL46.GL_FRAGMENT_SHADER));
 		this.shaderProgram.init();
+		
+			// GUI shaders
+		this.shaderProgramGUI = new ShaderProgram();
+		this.shaderProgramGUI.declareUniform(Renderer.U_PROJECTION_GUI);
+		this.shaderProgramGUI.declareUniform(Renderer.U_DIFFUSE_SAMPLER_GUI);
+		this.shaderProgramGUI.declareUniform(Renderer.U_OBJECT_TRANSFORM_GUI);
+		this.shaderProgramGUI.declareUniform(Renderer.U_HAS_TEXTURE_GUI);
+		this.shaderProgramGUI.declareUniform(Renderer.U_ELEMENT_COLOR_GUI);
+		this.shaderProgramGUI.declareUniform(Renderer.U_TEXT_COLOR_GUI);
+		this.shaderProgramGUI.addShader(new Shader("gui.vert", GL46.GL_VERTEX_SHADER));
+		this.shaderProgramGUI.addShader(new Shader("gui.frag", GL46.GL_FRAGMENT_SHADER));
+		this.shaderProgramGUI.init();
 		
 			// Initialize scene graphics assets
 		this.scene.init();
@@ -47,44 +79,63 @@ public class Renderer {
 		
 	public void render() {
 		GL46.glClear(GL46.GL_COLOR_BUFFER_BIT | GL46.GL_DEPTH_BUFFER_BIT);
+		
+			// Scene render pass
+		GL46.glEnable(GL46.GL_DEPTH_TEST);
 		this.shaderProgram.bind();
-		this.shaderProgram.setDiffuseSamplerUniform(0);
+		this.shaderProgram.setInteger1Uniform(Renderer.U_DIFFUSE_SAMPLER, 0);
 		
 		Camera activeCamera = this.scene.getActiveCamera();
 		activeCamera.getProjection().update(
 			this.clientWindow.getWidth(), this.clientWindow.getHeight()
 		);
 		
-		this.shaderProgram.setProjectionUniform(activeCamera.getProjection().getMatrix());
-		this.shaderProgram.setCameraTransformUniform(activeCamera.getCameraTransform());
+		this.shaderProgram.setMatrix4fUniform(
+			Renderer.U_PROJECTION, activeCamera.getProjection().getMatrix()
+		);
+		
+		this.shaderProgram.setMatrix4fUniform(
+			Renderer.U_CAMERA_TRANSFORM, activeCamera.getCameraTransform()
+		);
 			
-				// Width and height are not yet updated when resizing the window
-			GL46.glViewport(
-				0, 0, this.clientWindow.getWidth(), this.clientWindow.getHeight()
-			);
-			
-			for( ASceneObject object : this.scene.getObjects() ) {
+			// Width and height are not yet updated when resizing the window
+		GL46.glViewport(
+			0, 0, this.clientWindow.getWidth(), this.clientWindow.getHeight()
+		);
+		
+		for( ASceneObject object : this.scene.getObjects() ) {
+				// Determine the appropriate way of rendering the object
+				// (THIS MUST BE CHANGED TO A MORE DYNAMIC APPROACH)
+			if( object instanceof Model ) {
+				this.shaderProgram.setMatrix4fUniform(
+					Renderer.U_OBJECT_TRANSFORM, object.getTransformMatrix()
+				);
 				
-					// Determine the appropriate way of rendering the object
-					// (THIS MUST BE CHANGED TO A MORE DYNAMIC APPROACH)
-				if( object instanceof Model ) {
-					this.shaderProgram.setObjectTransformUniform(object.getTransformMatrix());
-					
-					Model model = (Model) object;
-					Texture texture = model.getTexture();
-					Mesh mesh = model.getMesh();
-					
-					this.textureCache.generateIfNotEncountered(texture);
-					GL46.glActiveTexture(GL46.GL_TEXTURE0);
-					texture.bind();
-					
-					VAO vao = this.vaoCache.getOrGenerate(mesh);
-					vao.bind();
-					GL46.glDrawElements(GL46.GL_TRIANGLES, vao.getVertexCount() * 3, GL46.GL_UNSIGNED_INT, 0);
-				}
+				Model model = (Model) object;
+				Texture texture = model.getTexture();
+				Mesh mesh = model.getMesh();
+				
+				this.textureCache.generateIfNotEncountered(texture);
+				GL46.glActiveTexture(GL46.GL_TEXTURE0);
+				texture.bind();
+				
+				VAO vao = this.vaoCache.getOrGenerate(mesh);
+				vao.bind();
+				GL46.glDrawElements(
+					GL46.GL_TRIANGLES, vao.getVertexCount() * 3, GL46.GL_UNSIGNED_INT, 0
+				);
 			}
+		}
 		
 		this.shaderProgram.unbind();
+		
+			// GUI render pass
+		GL46.glDisable(GL46.GL_DEPTH_TEST);
+		this.shaderProgramGUI.bind();
+		
+		
+		
+		this.shaderProgramGUI.unbind();
 	}
 	
 	public Window getClientWindow() {
--- 3drenderer/src/main/java/project/shader/ShaderProgram.java
+++ 3drenderer/src/main/java/project/shader/ShaderProgram.java
@@ -1,6 +1,8 @@
 package project.shader;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.joml.Matrix4f;
@@ -13,23 +15,22 @@ public class ShaderProgram {
 
 	private int programHandle;
 	private Map<String, Integer> uniformLocationMap;
+	private List<Shader> shaders;
 
 	public ShaderProgram() {
 		this.programHandle = -1;
 		this.uniformLocationMap = new HashMap<>();
+		this.shaders = new ArrayList<>();
 	}
 	
 	public void init() {
 		this.programHandle = GL46.glCreateProgram();
 		
-		Shader vertexShader = new Shader("default.vert", GL46.GL_VERTEX_SHADER);
-		Shader fragmentShader = new Shader("default.frag", GL46.GL_FRAGMENT_SHADER);
-		
-		vertexShader.init();
-		vertexShader.attach(this);
-		
-		fragmentShader.init();
-		fragmentShader.attach(this);
+		DebugUtils.log(this, this.shaders.size());		
+		for( Shader shader : this.shaders ) {
+			shader.init();
+			shader.attach(this);
+		}
 		
 		GL46.glLinkProgram(this.programHandle);	
 		
@@ -38,24 +39,35 @@ public class ShaderProgram {
 			return;
 		}
 		
-		vertexShader.detach(this);
-		fragmentShader.detach(this);
-		
-		this.createUniform("uDiffuseSampler");
-		this.createUniform("uProjection");
-		this.createUniform("uCameraTransform");
-		this.createUniform("uObjectTransform");
-	}
-	
-	private void createUniform(String name) {
-		final char START = 'u';
-		if( name.charAt(0) != START ) {
-			DebugUtils.log(this, "ERROR: Trying to declare a uniform with an invalid name! Name: '" + name + "'");
-			throw new RuntimeException("FATAL ERROR: Trying to create a uniform whose name doesn't start with '" + START + "'!");
+		for( Shader shader : this.shaders ) {
+			shader.detach(this);
 		}
 		
-		int uniformLocation = GL46.glGetUniformLocation(this.programHandle, name);
-		this.uniformLocationMap.put(name, uniformLocation);
+		for( Map.Entry<String, Integer> en : this.uniformLocationMap.entrySet() ) {
+			String key = en.getKey();
+			this.uniformLocationMap.put(
+				key, GL46.glGetUniformLocation(this.programHandle, key)
+			);
+		}
+	}
+	
+	public ShaderProgram declareUniform(String uniformName) {
+		final char START = 'u';
+		if( uniformName.charAt(0) != START ) {
+			DebugUtils.log(
+				this, 
+				"ERROR: Trying to declare a uniform with an invalid name! Name: '" + 
+				uniformName + "'"
+			);
+			
+			throw new RuntimeException(
+				"FATAL ERROR: Trying to create a uniform whose name doesn't start " +
+				"with '" + START + "'!"
+			);
+		}
+		
+		this.uniformLocationMap.put(uniformName, -1);
+		return this;
 	}
 	
 	public void bind() {
@@ -66,11 +78,17 @@ public class ShaderProgram {
 		GL46.glUseProgram(0);
 	}
 	
+	public void addShader(Shader shader) {
+		this.shaders.add(shader);
+	}
+	
 	private Integer getUniformOrError(String name) {
 		Integer uniformLocation = this.uniformLocationMap.get(name);
 		
 		if( uniformLocation == null ) {
-			throw new RuntimeException("ERROR: Trying to get a non-existent uniform '" + name + "'!");
+			throw new RuntimeException(
+				"ERROR: Trying to get a non-existent uniform '" + name + "'!"
+			);
 		}
 		
 		return uniformLocation;
@@ -88,22 +106,6 @@ public class ShaderProgram {
 				mat4f.get(stack.mallocFloat(16))
 			);
 		}
-	}
-	
-	public void setDiffuseSamplerUniform(int sampler) {
-		this.setInteger1Uniform("uDiffuseSampler", sampler);
-	}
-	
-	public void setProjectionUniform(Matrix4f projectionMatrix) {
-		this.setMatrix4fUniform("uProjection", projectionMatrix);
-	}
-	
-	public void setObjectTransformUniform(Matrix4f transformMatrix) {
-		this.setMatrix4fUniform("uObjectTransform", transformMatrix);
-	}
-	
-	public void setCameraTransformUniform(Matrix4f cameraTransformMatrix) {
-		this.setMatrix4fUniform("uCameraTransform", cameraTransformMatrix);
 	}
 	
 	public int getHandle() {
